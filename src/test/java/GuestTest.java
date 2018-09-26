813f54327d3eed341e18f29080b6279aa3fa2107
Fiona Wilson <fionaw237@gmail.com>
2018-09-26 10:16:18 +0100

    Test passed - guest has a name

--- src/test/java/GuestTest.java
+++ src/test/java/GuestTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void before() {
+        guest = new Guest("John Mike");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("John Mike", guest.getName());
+    }
+
 }
