573c36631a897002268a5fa37296d1f235ceb572
Fiona Wilson <fionaw237@gmail.com>
2018-09-26 10:26:41 +0100

    Test passed - bedroom has a capacity

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private String roomNumber;
+    private String type;
+    private double nightlyRate;
+
+    public Bedroom(int capacity, String roomNumber, String type, double nightlyRate) {
+        this.capacity = capacity;
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.nightlyRate = nightlyRate;
+        this.guests = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getNightlyRate() {
+        return nightlyRate;
+    }
 }
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+    Bedroom bedroom;
+
+    @Before
+    public void before() {
+        bedroom = new Bedroom(2, "101", "double", 40.20);
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(2, bedroom.getCapacity());
+    }
 }
