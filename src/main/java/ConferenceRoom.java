ff439d5b810530e0f1ce561a0fa69037e7275577
Fiona Wilson <fionaw237@gmail.com>
2018-09-26 10:39:33 +0100

    Adds ConferenceRoom class and tests

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private String name;
+    private double dailyRate;
+
+    public ConferenceRoom(int capacity, String name, double dailyRate) {
+        this.capacity = capacity;
+        this.name = name;
+        this.dailyRate = dailyRate;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDailyRate() {
+        return dailyRate;
+    }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before() {
+        conferenceRoom = new ConferenceRoom(10, "Meeting Room 1", 100);
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(10, conferenceRoom.getCapacity());
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Meeting Room 1", conferenceRoom.getName());
+    }
+
+    @Test
+    public void hasDailyRate() {
+        assertEquals(100, conferenceRoom.getDailyRate(), 0.01);
+    }
 }
